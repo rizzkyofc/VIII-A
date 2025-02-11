3ab4c1f381be212bcd345be37cf9fcfeb166a022
RizzKyxF <147376009+rizzkyofc@users.noreply.github.com>
2025-02-11 14:47:11 +0700

    Update Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -5,44 +5,22 @@ const Kamis = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TJKT Cloud-NAS</div>
-				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
+				<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+				<div className="flex justify-center items-center text-sm">7:40-9:00</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">Istirahat</div>
-				<div className="flex justify-center items-center text-sm">09.40-10.00</div>
+				<div className="w-[50%] text-base  font-medium">Ppkn</div>
+				<div className="flex justify-center items-center text-sm">9:40-12:00</div>
 			</div>
 			<div
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">PKK</div>
-				<div className="flex justify-center items-center text-sm">10.00-11.20</div>
-			</div>
-			<div
-				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
-				data-aos="fade-up"
-				data-aos-duration="800">
-				<div className="w-[50%] text-base  font-medium">Istirahat</div>
-				<div className="flex justify-center items-center text-sm">11.20-12.20</div>
-			</div>
-			<div
-				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
-				data-aos="fade-up"
-				data-aos-duration="800">
-				<div className="w-[50%] text-base  font-medium">TJKT Linux</div>
-				<div className="flex justify-center items-center text-sm">12.20-13.40</div>
-			</div>
-
-			<div
-				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
-				data-aos="fade-up"
-				data-aos-duration="1000">
-				<div className="w-[50%] text-base font-medium">B.Inggris</div>
-				<div className="flex justify-center items-center text-sm">13.40-15.00</div>
+				<div className="w-[50%] text-base  font-medium">Paibp</div>
+				<div className="flex justify-center items-center text-sm">12:00-14:30</div>
 			</div>
 		</>
 	)
