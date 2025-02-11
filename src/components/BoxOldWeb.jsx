ea05159c4577fb7216057c661f25e1350f35c84a
RizzKyxF <147376009+rizzkyofc@users.noreply.github.com>
2025-02-11 14:30:35 +0700

    Update BoxOldWeb.jsx

--- src/components/BoxOldWeb.jsx
+++ src/components/BoxOldWeb.jsx
@@ -1,7 +1,7 @@
 const BoxOldWeb = () => {
 	return (
 		<div id="BoxOldWeb">
-			<a href="#">
+			<a href="https://www.tiktok.com/@spenzel.8a?_t=ZS-8tox9idUplP&_r=1">
 				<div className="flex justify-between">
 					<img src="/link.png" alt="" className="w-10 h-10" />
 					<img src="/next.png" alt="" className="h-3 w-3" />
