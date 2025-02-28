9ad4bcf4bff249ae418d6daaeab4d90edae2dde3
RizzKyxF <147376009+rizzkyofc@users.noreply.github.com>
2025-02-28 07:56:51 +0700

    Update Kamis.jsx

--- src/components/Mapel/Kamis.jsx
+++ src/components/Mapel/Kamis.jsx
@@ -5,21 +5,21 @@ const Kamis = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+				<div className="w-[50%] text-base  font-medium">Bahasa Inggris</div>
 				<div className="flex justify-center items-center text-sm">7:40-9:00</div>
 			</div>
 			<div
 				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">Ppkn</div>
+				<div className="w-[50%] text-base  font-medium">Pendidikan Pacasila Dan Kewarganegaraan</div>
 				<div className="flex justify-center items-center text-sm">9:40-12:00</div>
 			</div>
 			<div
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">Paibp</div>
+				<div className="w-[50%] text-base  font-medium">Pendidikan Agama Islam Dan Budi Pekerti</div>
 				<div className="flex justify-center items-center text-sm">12:00-14:30</div>
 			</div>
 		</>
