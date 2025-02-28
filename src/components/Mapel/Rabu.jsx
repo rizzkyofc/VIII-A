27daec4097f76d248e9c62428e2978aeca543bbb
RizzKyxF <147376009+rizzkyofc@users.noreply.github.com>
2025-02-28 07:55:41 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -2,19 +2,19 @@ const Rabu = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Ipa</div>
+					<div className="w-[50%] text-base  font-medium">Ilmu Pengetahuan Alam</div>
 					<div className="flex justify-center items-center text-sm">07:40-9:00</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">B.Inggris</div>
+					<div className="w-[50%] text-base  font-medium">Bahasa Inggris</div>
 					<div className="flex justify-center items-center text-sm">9:00-10:40</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-					<div className="w-[50%] text-base  font-medium">B.Sunda</div>
+					<div className="w-[50%] text-base  font-medium">Bahasa Sunda</div>
 					<div className="flex justify-center items-center text-sm"></div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">Ips</div>
+					<div className="w-[50%] text-base  font-medium">Ilmu Pengetahuan Sosial</div>
 					<div className="flex justify-center items-center text-sm"></div>
 				</div>
 			
