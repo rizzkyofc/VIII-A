b2f67f2388578d3dc93ef0ce200852e69178a2cc
RizzKyxF <147376009+rizzkyofc@users.noreply.github.com>
2025-02-28 07:54:53 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -2,11 +2,11 @@ const Selasa = () => {
     return (
         <>
         	<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Tik</div>
+					<div className="w-[50%] text-base  font-medium">Teknologi Informasi Dan Komunikasi</div>
 					<div className="flex justify-center items-center text-sm">07:00-08:20</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Ipa</div>
+					<div className="w-[50%] text-base  font-medium">Ilmu Pengetahuan Alam</div>
 					<div className="flex justify-center items-center text-sm">08:20-10:00</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -19,7 +19,7 @@ const Selasa = () => {
 				</div>
 
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="900">
-					<div className="w-[50%] text-base  font-medium">B.Indonesia</div>
+					<div className="w-[50%] text-base  font-medium">Bahsa Indonesia</div>
 					<div className="flex justify-center items-center text-sm">12:30-14:30</div>
 				</div>
         </>
