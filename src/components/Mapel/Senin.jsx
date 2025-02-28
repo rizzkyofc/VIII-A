fafa71d92fc5bb334342e41aca68e8885e91c696
RizzKyxF <147376009+rizzkyofc@users.noreply.github.com>
2025-02-28 07:53:54 +0700

    Update Senin.jsx

--- src/components/Mapel/Senin.jsx
+++ src/components/Mapel/Senin.jsx
@@ -6,7 +6,7 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm">07:40-09.40</div>
 				</div>
 				<div className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60" data-aos="fade-up" data-aos-duration="600">
-					<div className="w-[50%] text-base  font-medium">Pjok</div>
+					<div className="w-[50%] text-base  font-medium">Pendidikan Jasmani Olahraga Dan Kesehatan</div>
 					<div className="flex justify-center items-center text-sm">10:00-12:00</div>
 				</div>
 				<div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -14,7 +14,7 @@ const Senin = () => {
 					<div className="flex justify-center items-center text-sm"></div>
 				</div>
 				<div className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-					<div className="w-[50%] text-base  font-medium">Ips</div>
+					<div className="w-[50%] text-base  font-medium">Ilmu Pengetahuan Sosial</div>
 					<div className="flex justify-center items-center text-sm">12:30-13:50</div>
 				</div>
 
